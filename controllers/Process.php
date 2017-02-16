54d16db10a9445cefe649083555eeed1d347c46a
JSepulvedaX <jsepulvedax@gmail.com>
2017-02-16 11:18:33 -0300

    cambio de formato del mensaje hacia el socket

--- controllers/Process.php
+++ controllers/Process.php
@@ -66,7 +66,7 @@ class Process extends CI_Controller {
             die("Could not connect: [$errorcode] $errormsg \n");
         }
 
-        $message = "[use_id],".$lat.",".$lng.",2015-01-01,2015-12-31";
+        $message = "#user_id,".$lat.",".$lng.",2015-01-01,2015-12-31";
 
         //Send the message to the server
         if(!socket_send( $sock , $message , strlen($message) , 0))
